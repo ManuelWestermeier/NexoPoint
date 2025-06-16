8b3892e4bc5162a154783ffb5a414559a1810e7b
manuelwestermeier <westermeier111@gmail.com>
2025-06-16 19:36:51 +0200

    .

---
+++ frontend/vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "NexoPoint",
+        short_name: "Nexo",
+        description:
+          "NexoPoint lets you share data securely via local Bluetooth share points.",
+        theme_color: "#0D1321",
+        background_color: "#F7F9FB",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+  build: { outDir: "../docs" },
+});
